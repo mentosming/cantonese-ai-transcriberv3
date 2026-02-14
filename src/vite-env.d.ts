d98c790e98e10c4c447a23be3936627c9ced1b23
mentosming <ming1988@gmail.com>
2026-02-15 02:34:19 +0800

    refactor: update importmap in index.html and clean up vite-env.d.ts for better module handling

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,23 +1,10 @@
+/// <reference types="vite/client" />
+
 declare module '*.svg' {
   import * as React from 'react';
   export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-  const content: string;
-  export default content;
-}
-
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.json' {
-  const content: string;
-  export default content;
+  const src: string;
+  export default src;
 }
 
 declare namespace NodeJS {
