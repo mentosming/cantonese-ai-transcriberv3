23439bacbb232b934eea30d14f5096984fe76860
mentosming <ming1988@gmail.com>
2026-02-15 02:25:57 +0800

    fix: standardize variable naming for asset modules in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,23 +1,23 @@
 declare module '*.svg' {
   import * as React from 'react';
   export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-  const src: string;
-  export default src;
+  const content: string;
+  export default content;
 }
 
 declare module '*.jpg' {
-  const src: string;
-  export default src;
+  const content: string;
+  export default content;
 }
 
 declare module '*.png' {
-  const src: string;
-  export default src;
+  const content: string;
+  export default content;
 }
 
 declare module '*.json' {
-  const src: string;
-  export default src;
+  const content: string;
+  export default content;
 }
 
 declare namespace NodeJS {
